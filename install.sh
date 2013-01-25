87f794e9bb09d7768f51bdffbd3d5babf2d17c6a
alexsimonjones@gmail.com <alexsimonjones@gmail.com>
2013-01-25 10:55:14 +0000

    checks for vundle

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash +x
+DIRECTORY="~/.vimrc/bundle/vundle"
 
 if [ "$(id -u)" == "0" ]; then
 
 	echo "This must not be run as root!"
 	exit 1
+fi
+
+if [ ! -d "$DIRECTORY" ]; then
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 fi
 
 cp ~/.vimrc old_vimrc_backup
