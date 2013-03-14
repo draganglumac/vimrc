bce14b3f8de5db015388e10f76cd5dbb298abc98
Caspar <admin@caspar.local>
2013-03-14 09:28:17 +0000

    fixed broken logic for checking if vundle is installed

--- install.sh
+++ install.sh
@@ -7,11 +7,8 @@ if [ "$(id -u)" == "0" ]; then
 	exit 1
 fi
 
-if [ ! -d "$DIRECTORY" ]; then
-
-    if [ $? -ne 0 ]; then
+if [ ! -e "$DIRECTORY" ]; then
     git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-    fi
 fi
 
 cp ~/.vimrc old_vimrc_backup
